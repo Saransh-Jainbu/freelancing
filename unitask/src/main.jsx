bfbbf22b1dd9a913a8e7d79e221a936f4971e7b8
Saransh Jain <saranshj647@gmail.com>
2025-03-16 08:48:54 +0530

    add Tailwind CSS configuration and update project dependencies; set up PostCSS and Vite for styling

--- unitask/src/main.jsx
+++ unitask/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css';
+import './index.css'; // Remove ?inline as it's not needed with updated config
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
